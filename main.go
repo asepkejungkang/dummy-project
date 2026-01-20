d58691f913d203fc1cd4086f1706a990c321738b
asepkejungkang <yamisokv6@gmail.com>
2026-01-20 11:09:15 +0700

    implement release

--- main.go
+++ main.go
@@ -10,6 +10,10 @@ func main() {
 		fmt.Fprintf(w, "Hello, World!")
 	})
 
+	http.HandleFunc("/release", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Release")
+	})
+
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic("error listening 8080")
 	}
