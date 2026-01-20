8319f9247dbaa7c9d76cbed611f273dbb0fda877
asepkejungkang <yamisokv6@gmail.com>
2026-01-20 11:08:26 +0700

    implement playlist

--- main.go
+++ main.go
@@ -10,6 +10,10 @@ func main() {
 		fmt.Fprintf(w, "Hello, World!")
 	})
 
+	http.HandleFunc("/playlist", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Playlist")
+	})
+
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		panic("error listening 8080")
 	}
